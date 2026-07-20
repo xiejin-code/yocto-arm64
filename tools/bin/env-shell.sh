b2eeec4095570b1f23c52417cda0315870ab0883
jin xie <jin.xie@qq.com>
2026-07-20 15:55:56 +0800

    Delete unnecessary information.

--- tools/bin/env-shell.sh
+++ tools/bin/env-shell.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 for file in "${BUILDDIR}/../../../tools/env-shell.d/"*; do
-    echo "Sourcing ${file}"
     . "${file}"
 done
--- tools/env-shell.d/bashrc.sh
+++ tools/env-shell.d/bashrc.sh
@@ -3,9 +3,6 @@
 # bashrc.sh - Custom Bash prompt and shell settings
 #
 
-# Load Git prompt helper for branch display
-#source /usr/share/git/git-prompt.sh 2>/dev/null
-
 # Custom PS1: user@host:path (branch)$
 # Green user@host, blue path, yellow git branch
 PS1='\[\e[01;32m\]$(__git_ps1 " (YOCTO [%s])" 2>/dev/null) \u@\h:\w\n\$ \[\e[00m\]'
